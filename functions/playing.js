0b0a1acadd018dbf26bf539182a0e9e81bc2ed47
gitrog <37446909+gitrogdev@users.noreply.github.com>
2025-12-26 16:07:24 -0500

    Log basic gameplay actions

--- functions/playing.js
+++ functions/playing.js
@@ -30,6 +30,7 @@ function presentClip(user) {
 	if (!(user.id in sessions)) return;
 	const sessionInfo = sessions[user.id];
 
+	console.log(`Presenting clip${sessionInfo.clip}.mp3 to ${user.globalName}`);
 	user.send({
 		content: `**Clip ${sessionInfo.clip}**:\n*Use /guess to guess the song `
 			+ ' or type /skip to try a longer clip before guessing.*',
@@ -48,6 +49,7 @@ function presentClip(user) {
  * @param {import('discord.js').User} user the user to finish the session for
  */
 function finish(user) {
+	console.log(`${user.globalName} has finished a round of Yorkle.`);
 	finishGame(user, sessions[user.id]);
 	delete sessions[user.id];
 };
@@ -80,6 +82,9 @@ module.exports.makeGuess = function(user, guess) {
 	const sessionInfo = sessions[user.id];
 
 	const cleanGuess = guess.toLowerCase().replace(/[\s.,\-()/?!]/g, '');
+	console.log(
+		`${user.globalName} has guessed "${guess}" (cleaned to "${cleanGuess}")`
+	);
 	if (!validAliases.has(cleanGuess)) return `Unknown song "${guess}"\nTry `
 		+ 'checking for spelling mistakes, or try another song. Answers are '
 		+ 'not case or punctuation dependent.';
@@ -121,6 +126,7 @@ module.exports.skip = async function(user) {
 	if (sessionInfo.clip >= guessLengths.length)
 		return 'This is my final clip! Make a guess!';
 
+	console.log(`${user.globalName} is skipping clip ${sessionInfo.clip}.`);
 	sessionInfo.guesses.push('-');
 	sessionInfo.clip++;
 	presentClip(user);
@@ -144,6 +150,7 @@ module.exports.start = async function(user) {
 		return false;
 	}
 
+	console.log(`${user.globalName} has started a round of Yorkle.`);
 	sessions[user.id] = {
 		answer: songData.song.slice(0, -4),
 		clip: 1,
