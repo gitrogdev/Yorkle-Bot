6574c7682468ffa46b2b8a5a08ba2b6bbdaa4630
gitrog <37446909+gitrogdev@users.noreply.github.com>
2025-12-27 07:52:26 -0500

    Add disclaimer about alternative songs

--- commands/content.js
+++ commands/content.js
@@ -19,6 +19,12 @@ module.exports = {
 		await interaction.reply(
 			`Yorkle currently recognizes \`${COUNT}\` songs from Radiohead `
 			+ 'studio albums, B-sides, singles, and other releases.\n\n'
+			+ 'Live versions, acoustic versions, remasters, remixes, or any '
+			+ 'other alternative versions of songs are not included.\n\n'
+			+ '*i.e. Players will not have to distinguish between* **True Love '
+			+ 'Waits** *and* **True Love Waits (Live in Oslo)**, *but they '
+			+ 'will have to distinguish between* **Morning Bell** *and* '
+			+ '**Morning Bell/Amnesiac**.\n\n'
 			+ '-# More content will be added over time, potentially including '
 			+ 'solo work, as well as On a Friday, The Smile and Atoms for '
 			+ 'Peace.'
