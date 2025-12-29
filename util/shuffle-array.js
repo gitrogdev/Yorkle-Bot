7d102a1b3c9ba0ab34b038bb93159c4fdf382543
gitrog <37446909+gitrogdev@users.noreply.github.com>
2025-12-29 13:34:55 -0500

    Implement a better shuffling method

--- util/shuffle-array.js
+++ util/shuffle-array.js
@@ -4,10 +4,10 @@
  * @param {Array} array the reference to the array to shuffle
  */
 module.exports = function(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	};
+};
