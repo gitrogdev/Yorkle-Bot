04b02b3243c4d251f59c9bdf8bdbd2ea9f921116
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-27 08:53:47 +0100

    Validate aliases on registry

--- src/persistence/AliasRegistry.ts
+++ src/persistence/AliasRegistry.ts
@@ -1,4 +1,5 @@
 import aliases from '../config/aliases.json' with { type: 'json' };
+import cleanTitle from '../util/clean-song.js';
 
 export default class AliasRegistry {
 	private static aliases: Set<string>;
@@ -10,8 +11,15 @@ export default class AliasRegistry {
 		AliasRegistry.aliases = new Set<string>();
 
 		for (const songAliases of Object.values(aliases))
-			for (const alias of songAliases) AliasRegistry.aliases.add(alias);
-
-		console.log(AliasRegistry.aliases);
+			for (const alias of songAliases) {
+				const cleanAlias = cleanTitle(alias);
+				if (cleanAlias !== alias) console.warn(
+					`The alias "${alias}" is not clean in in aliases.json!`
+				);
+				if (AliasRegistry.aliases.has(cleanAlias)) console.warn(
+					`Multiple instances of the alias "${cleanAlias}" appear in `
+					+ 'aliases.json!'
+				); else AliasRegistry.aliases.add(cleanAlias);
+			}
 	}
 }
