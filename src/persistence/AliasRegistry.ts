df59f69357cd80d9cb51a4bca4f321d322476a7d
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-01-27 17:17:09 +0100

    Restructure AliasRegistry to be a dictionary of aliases to Songs

--- src/persistence/AliasRegistry.ts
+++ src/persistence/AliasRegistry.ts
@@ -1,8 +1,12 @@
 import aliases from '../config/aliases.json' with { type: 'json' };
+import type Song from '../game/entities/Song.js';
 import cleanTitle from '../util/clean-song.js';
+import SongLibrary from './SongLibrary.js';
 
 export default class AliasRegistry {
-	private static aliases: Set<string>;
+	private static aliases: {
+		[alias: string]: Song
+	};
 
 	/**
 	 * Loads all the aliases from /config/aliases.json as a set.
@@ -11,18 +15,20 @@ export default class AliasRegistry {
 		if (AliasRegistry.aliases != null) throw new Error(
 			'AliasRegistry already loaded!'
 		);
-		AliasRegistry.aliases = new Set<string>();
+		AliasRegistry.aliases = {};
 
-		for (const songAliases of Object.values(aliases))
+		for (const [ songTitle, songAliases ] of Object.entries(aliases))
 			for (const alias of songAliases) {
 				const cleanAlias = cleanTitle(alias);
 				if (cleanAlias !== alias) console.warn(
 					`The alias "${alias}" is not clean in in aliases.json!`
 				);
-				if (AliasRegistry.aliases.has(cleanAlias)) console.warn(
+				if (cleanAlias in aliases) throw new Error(
 					`Multiple instances of the alias "${cleanAlias}" appear in `
 					+ 'aliases.json!'
-				); else AliasRegistry.aliases.add(cleanAlias);
+				); else AliasRegistry.aliases[cleanAlias] = SongLibrary.getSong(
+					songTitle
+				);
 			}
 	}
 
@@ -34,6 +40,6 @@ export default class AliasRegistry {
 	 * @returns {boolean} whether the guess is a recognized alias
 	 */
 	public static isValid(guess: string): boolean {
-		return AliasRegistry.aliases.has(guess);
+		return guess in AliasRegistry.aliases;
 	}
 }
