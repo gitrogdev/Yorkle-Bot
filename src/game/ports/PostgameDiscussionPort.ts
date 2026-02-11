e6abba5fb298c6477a6a790192445f4235e59045
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-02-11 17:32:14 +0100

    Add joinPostgameThreads to the PostgameDiscussionPort interface

--- src/game/ports/PostgameDiscussionPort.ts
+++ src/game/ports/PostgameDiscussionPort.ts
@@ -1,4 +1,5 @@
 import type Guild from '../entities/Guild.js';
+import type UserIdentity from '../model/UserIdentity.js';
 
 /**
  * Port interface for opening discussion threads on the client.
@@ -8,7 +9,20 @@ import type Guild from '../entities/Guild.js';
  *
  * @author gitrog
  */
-export default interface OpenDiscussionPort {
+export default interface PostgameDiscussionPort {
+	/**
+	 * Adds a user to all postgame discussion threads for a day's iteration of
+	 * the game.
+	 *
+	 * @author gitrog
+	 *
+	 * @param {UserIdentity} user a representation of the information of the
+	 * user to add to the threads
+	 * @param {number} day the number of the day of the game's iteration for the
+	 * user to join the threads of
+	 */
+	joinPostgameThreads(user: UserIdentity, day: number): Promise<void>
+
 	/**
 	 * Opens a thread for postgame discussion for the provided day.
 	 *
@@ -17,11 +31,8 @@ export default interface OpenDiscussionPort {
 	 * @param {Guild} guild the guild to open the discussion thread in
 	 * @param {number} day the number of the day of the game's iteration to
 	 * associate the thread with
-	 *
-	 * @returns {Promise<string>} a promise of the ID of the discussion thread
-	 * created
 	 */
-	openPostgameThread(guild: Guild, day: number): Promise<string>;
+	openPostgameThread(guild: Guild, day: number): Promise<void>;
 
 	/**
 	 * Passes the thread for an already open postgame discussion to the client
