bc397ab8565565575b4c6506fddd0c6d35577737
gitrog <37446909+gitrogdev@users.noreply.github.com>
2026-03-19 19:41:30 +0100

    Implement the /hint command

---
+++ src/presentation/discord/commands/HintCommand.ts
@@ -0,0 +1,34 @@
+import {
+	ChatInputCommandInteraction,
+	InteractionContextType,
+	SlashCommandBuilder,
+	type RESTPostAPIChatInputApplicationCommandsJSONBody
+} from 'discord.js';
+import Command from '../models/Command.js';
+import { getLocalizedOptions, localize } from '../../localization/i18n.js';
+
+export default class HintCommand extends Command {
+	/** The JSON payload used to register the command with the Discord API. */
+	public readonly data: RESTPostAPIChatInputApplicationCommandsJSONBody =
+		new SlashCommandBuilder()
+			.setName(localize('commands.hint'))
+			.setNameLocalizations(getLocalizedOptions('commands.hint'))
+			.setDescription(localize('commands.hint.description'))
+			.setDescriptionLocalizations(
+				getLocalizedOptions('commands.hint.description')
+			).setContexts(
+				InteractionContextType.BotDM
+			).toJSON();
+
+	/**
+	 * Executes the command.
+	 *
+	 * @author gitrog
+	 *
+	 * @param {ChatInputCommandInteraction} interaction the Discord chat command
+	 * interaction with the command
+	 */
+	public async run(interaction: ChatInputCommandInteraction) {
+		return await this.handler.requestHint(interaction);
+	}
+}
